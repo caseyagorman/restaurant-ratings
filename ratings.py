2f7b20d50f867766574226663310003b2d085d8a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 13:55:34 -0700

    Added write_to_file function

--- ratings.py
+++ ratings.py
@@ -1,7 +1,7 @@
 def print_dictionary(current_ratings):
 	current_ratings = sorted(current_ratings.items())
 	for restaurant, rating in current_ratings:
-		print(restaurant + " is rated at a " + str(rating) + ".")
+		print(restaurant + " is rated at a " + rating + ".")
 
 
 def clean_ratings(filename):
@@ -18,44 +18,43 @@ def clean_ratings(filename):
 	return dict(ratings)
 
 
+# def get_new_rating(original_ratings):
+# 	new_restaurant = input("What would you like to enter? ")
+# 	new_restaurant = new_restaurant.capitalize()
+# 	try:
+# 		new_score = int(input("What would you score it as? "))
+
+# 		if type(new_score) == int and new_score >= 1 and new_score <= 5:
+
+# 			original_ratings[new_restaurant] = new_score
+# 			return original_ratings
+
+# 		else:
+# 			print("Please enter a valid rating. ")
+# 			get_new_rating(ratings)
+# 	except ValueError:
+# 		print("Please enter a number")
+# 		get_new_rating(ratings)
+
+
+
+
 def get_new_rating(original_ratings):
-	new_restaurant = input("What would you like to enter? ")
-	new_restaurant = new_restaurant.capitalize()
-	try:
-		new_score = int(input("What would you score it as? "))
+	new_restaurant = (input("What would you like to enter? ")).capitalize()
+	new_score = (input("What would you score it as? "))
 
-		if type(new_score) == int and new_score >= 1 and new_score <= 5:
+	valid_scores = ["1", "2", "3", "4", "5"]
 
-			original_ratings[new_restaurant] = new_score
-			return original_ratings
+	if new_score in valid_scores:
 
-		else:
-			print("Please enter a valid rating. ")
-			get_new_rating(ratings)
-	except ValueError:
-		print("Please enter a number")
+		original_ratings[new_restaurant] = new_score
+		return original_ratings
+		
+	else:
+		print("Please enter a valid rating. ")
 		get_new_rating(ratings)
 
 
-
-
-# def get_new_rating(original_ratings):
-# 	new_restaurant = input("What would you like to enter? ")
-# 	new_score = (input("What would you score it as? "))
-
-# 	valid_scores = ["1", "2", "3", "4", "5"]
-
-# 	if new_score in valid_scores:
-
-# 		original_ratings[new_restaurant] = new_score
-# 		original_ratings = sorted(original_ratings.items())
-			
-# 		for restaurant, rating in original_ratings:
-# 			print(restaurant + " is rated at a " + (rating) + ".")
-		
-# 	else:
-# 		print("Please enter a valid rating. ")
-# 		get_new_rating(ratings)
 
 def restaurant_ratings(filename):
 	ratings = clean_ratings(filename)
