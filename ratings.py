cabd2c021863455f90557f17e829ba0e0651c0b8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-29 12:30:26 -0700

    Add get_new_rating fucntion

--- ratings.py
+++ ratings.py
@@ -1,3 +1,6 @@
+def print_dictionary():
+	pass
+
 def rating(filename):
 	my_file = open(filename)
 	ratings = {}
@@ -14,14 +17,45 @@ def rating(filename):
 
 def get_new_rating(original_ratings):
 	new_restaurant = input("What would you like to enter? ")
-	new_score = input("What would you score it as? ")
+	new_restaurant = new_restaurant.capitalize()
+	try:
+		new_score = int(input("What would you score it as? "))
 
-	original_ratings[new_restaurant] = new_score
+		if type(new_score) == int and new_score >= 1 and new_score <= 5:
 
-	original_ratings = sorted(original_ratings.items())
-	for restaurant, rating in original_ratings:
-		print(restaurant + " is rated at a " + rating + ".")
-	
+			original_ratings[new_restaurant] = new_score
+			new_ratings = sorted(original_ratings.items())
+
+			for restaurant, rating in new_ratings:
+				print(restaurant + " is rated at a " + str(rating) + ".")
+		else:
+			print("Please enter a valid rating. ")
+			get_new_rating(ratings)
+	except ValueError:
+		print("Please enter a number")
+		get_new_rating(ratings)
+
+
+
+
+# def get_new_rating(original_ratings):
+# 	new_restaurant = input("What would you like to enter? ")
+# 	new_score = (input("What would you score it as? "))
+
+# 	valid_scores = ["1", "2", "3", "4", "5"]
+
+# 	if new_score in valid_scores:
+
+# 		original_ratings[new_restaurant] = new_score
+# 		original_ratings = sorted(original_ratings.items())
+			
+# 		for restaurant, rating in original_ratings:
+# 			print(restaurant + " is rated at a " + (rating) + ".")
+		
+# 	else:
+# 		print("Please enter a valid rating. ")
+# 		get_new_rating(ratings)
+
 
 ratings = rating("scores.txt")
 get_new_rating(ratings)
